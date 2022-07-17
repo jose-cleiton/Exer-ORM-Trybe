4b536dc201ea92de99e7b4100eaa956aee28c261
jose-cleiton <cleitons835@gmai.com>
2022-07-17 18:58:02 -0300

    Feat : Criando um servidor simples

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.clear();
+    console.log(`Server is running on port ${port}`);
+})
